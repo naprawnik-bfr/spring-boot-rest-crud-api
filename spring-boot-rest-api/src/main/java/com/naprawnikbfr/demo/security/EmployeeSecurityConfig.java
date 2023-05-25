6d4c4368366822c08b1f99364a10e34e341bedb8
BLAZEJ FRACZEK <fraczek.blazej@gmail.com>
2023-05-25 23:37:02 +0200

    Commit after adding JDBC authentication.

--- spring-boot-rest-api/src/main/java/com/naprawnikbfr/demo/security/EmployeeSecurityConfig.java
+++ spring-boot-rest-api/src/main/java/com/naprawnikbfr/demo/security/EmployeeSecurityConfig.java
@@ -5,14 +5,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class EmployeeSecurityConfig {
 
+    //add support for JDBC...no more hard-coded users.
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){    // injecting data source autoconfigured by Spring Boot
+
+        return new JdbcUserDetailsManager(dataSource);                      // tell Spring Sec to use JDBC authentication with our data source
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+            configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        //we are customizing authentication, so we have to tell Spring we are using basic authentication.
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable CSRF
+        //in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    /* Previous - hard-coded users
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
@@ -36,25 +66,5 @@ public class EmployeeSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configurer ->
-            configurer
-                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-        );
-
-        //we are customizing authentication, so we have to tell Spring we are using basic authentication.
-        http.httpBasic(Customizer.withDefaults());
-
-        //disable CSRF
-        //in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
-        http.csrf(csrf -> csrf.disable());
-
-        return http.build();
-    }
+     */
 }
